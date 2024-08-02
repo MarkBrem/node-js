253809f27b01486535fbfe508f7c9c4f77992ac4
Марко <mgusanica@gmail.com>
2024-08-02 20:28:51 +0300

    авіаів

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-import colors from "./js/countries.json"
-import colorsTmpl from './temples/gallery-item.handlebars'
-const list = document.querySelector('.js-palette')
-list.insertAdjacentHTML("beforeend", colorsTmpl(colors))
+// const refs = {
+//     notification: document.querySelector('.js-alert'),
+//     };
+    
+//     refs.notification.addEventListener('click', onNotificationClick);
+    
+//     showNotification();
+
+//     const timeoutId = setTimeout(hideNotification, 3000)
+
+//     function onNotificationClick() {
+//     hideNotification();
+//     clearTimeout(timeoutId)
+//     }
+    
+//     function showNotification() {
+//     refs.notification.classList.add('is-visible');
+    
+//     }
+    
+//     function hideNotification() {
+//     refs.notification.classList.remove('is-visible');
+//     console.log('Закрили модалку');
+//     }
+ 
+
+setTimeout(()=>{confirm('Підпишись')}, 5000)
+
+let count = 0
+
+setInterval(()=>{
+    confirm('Підпишись')
+    count += 1;
+    console.log(count);
+}, 3000);
+
