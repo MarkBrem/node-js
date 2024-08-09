9d3e3e5b270cb9007ac7f37c27fd94f3bee1875c
Марко <mgusanica@gmail.com>
2024-08-09 20:33:07 +0300

    рррр

--- src/index.js
+++ src/index.js
@@ -23,14 +23,83 @@
 //     console.log('Закрили модалку');
 //     }
  
+// let count = 0
 
-setTimeout(()=>{confirm('Підпишись')}, 5000)
+// const subscribe = setInterval(()=>{
+//     confirm('Підпишись')
+//     if(confirm('Підпишись') === false){
+//         count += 1
+//     }else{
+//         clearInterval(subscribe)
+//     }
+//     console.log(count);
+//     if (count === 3) {
+//         clearInterval(subscribe)
+//     }
+// }, 3000);
 
-let count = 0
 
-setInterval(()=>{
-    confirm('Підпишись')
-    count += 1;
-    console.log(count);
-}, 3000);
 
+
+/**
+ * Напишіть функцію printNumbers(from, to) яка виводить число кожну секунду, 
+ * починаючи від from і закінчуючи to.
+    * Зробіть два варіанти рішення.
+    * 1. Використовуючи setInterval.
+    * 2. Використовуючи вкладений setTimeout.
+ */
+
+// function printNumbers(from, to){
+//     let start = from;
+    
+//     const timerId = setInterval(()=>{
+//         console.log(start);
+//         if (start === to) {
+//             clearInterval(timerId)
+//         } 
+//         start += 1
+//     }, 500)
+// }
+
+// printNumbers(1, 6)
+
+
+
+// function printNumbers(from, to){
+//     let start = from;
+
+//     function print() {
+//         console.log(start);
+//         if (start < to) {
+//           setTimeout(print, 1000);
+//         }
+//         start += 1;
+//       }
+    
+//       setTimeout(print, 500);
+//     }
+    
+//     printNumbers(1, 6);
+
+// 1. Створити функцію, яка повертає поточну дату та час.
+
+// const date = function(){
+//    console.log(new Date());
+//    return new Date()
+// }
+
+// 2. Створити функцію, яка приймає дату та повертає рік.
+
+// const year = function(date){
+//    console.log(date.getFullYear());
+// }
+// year(date())
+
+function findDate(){
+   const today = new Date()
+   const year = today.getFullYear()
+   const month = today.getMonth() + 1
+   const date = today.getDate()
+   console.log(year, month, date);
+}
+findDate()
