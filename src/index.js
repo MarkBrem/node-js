005494164418b42f0f898a7a5fa07ce0bded88b5
Марко <mgusanica@gmail.com>
2024-08-16 20:30:31 +0300

    ой так так

--- src/index.js
+++ src/index.js
@@ -95,11 +95,14 @@
 // }
 // year(date())
 
-function findDate(){
-   const today = new Date()
-   const year = today.getFullYear()
-   const month = today.getMonth() + 1
-   const date = today.getDate()
-   console.log(year, month, date);
-}
-findDate()
+// function findDate(){
+//    const today = new Date()
+//    const year = today.getFullYear()
+//    const month = today.getMonth() + 1
+//    const date = today.getDate()
+//    console.log(year, month, date);
+// }
+// findDate()
+
+
+
