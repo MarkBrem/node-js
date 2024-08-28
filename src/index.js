be01c95013f34fd39650b223f5d0b10dbdad6ece
Марко <mgusanica@gmail.com>
2024-08-28 17:25:51 +0300

    df

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // const refs = {
 //     notification: document.querySelector('.js-alert'),
 //     };
-    
+
 //     refs.notification.addEventListener('click', onNotificationClick);
-    
+
 //     showNotification();
 
 //     const timeoutId = setTimeout(hideNotification, 3000)
@@ -12,17 +12,17 @@
 //     hideNotification();
 //     clearTimeout(timeoutId)
 //     }
-    
+
 //     function showNotification() {
 //     refs.notification.classList.add('is-visible');
-    
+
 //     }
-    
+
 //     function hideNotification() {
 //     refs.notification.classList.remove('is-visible');
 //     console.log('Закрили модалку');
 //     }
- 
+
 // let count = 0
 
 // const subscribe = setInterval(()=>{
@@ -38,32 +38,27 @@
 //     }
 // }, 3000);
 
-
-
-
 /**
- * Напишіть функцію printNumbers(from, to) яка виводить число кожну секунду, 
+ * Напишіть функцію printNumbers(from, to) яка виводить число кожну секунду,
  * починаючи від from і закінчуючи to.
-    * Зробіть два варіанти рішення.
-    * 1. Використовуючи setInterval.
-    * 2. Використовуючи вкладений setTimeout.
+ * Зробіть два варіанти рішення.
+ * 1. Використовуючи setInterval.
+ * 2. Використовуючи вкладений setTimeout.
  */
 
 // function printNumbers(from, to){
 //     let start = from;
-    
+
 //     const timerId = setInterval(()=>{
 //         console.log(start);
 //         if (start === to) {
 //             clearInterval(timerId)
-//         } 
+//         }
 //         start += 1
 //     }, 500)
 // }
 
 // printNumbers(1, 6)
-
-
 
 // function printNumbers(from, to){
 //     let start = from;
@@ -75,10 +70,10 @@
 //         }
 //         start += 1;
 //       }
-    
+
 //       setTimeout(print, 500);
 //     }
-    
+
 //     printNumbers(1, 6);
 
 // 1. Створити функцію, яка повертає поточну дату та час.
@@ -103,6 +98,107 @@
 //    console.log(year, month, date);
 // }
 // findDate()
+// const promise = new Promise((resolve, reject)=>{
+//    resolve(10)
+// })
+
+// const promise = new Promise((resolve, reject) => {
+
+//     resolve('Hello, world!')
+// }
+// )
+
+// promise
+//     .then(result => { console.log(result); })
+//     .then(result => {console.log(result);})
+
+//. Напишіть функцію, яка отримує масив чисел
+// і повертає проміс, який виконується, якщо всі
+//числа в масиві є парними.Якщо ж принаймні одне число
+//непарне, проміс повинен бути відхилено з повідомленням
+//"Є непарні числа".
+
+// function arrChecking (arr) {
+//     const everyNum = arr.every((num) => {
+//      return   num % 2 === 0
+//     })
+//     return new Promise((resolve, reject) => {
+
+//         if (everyNum) {
+//             resolve('всі числа парні')
+//         } else {
+//             reject('Є непарні числа')
+
+//         }
+//     })
+// }
+
+// arrChecking([2, 4, 6])
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+// const promise1 =new Promise((resolve, reject) => {
+//     setTimeout(resolve('проміс 1 виконаний'), 3000);
+//   });
+
+// const promise2 =new Promise((resolve, reject) => {
+//     setTimeout(resolve('проміс 2 виконаний'), 2000);
+//   });
+
+// const promise3 =new Promise((resolve, reject) => {
+//     setTimeout(resolve('проміс 3 виконаний'), 1000);
+//   });
+
+// Promise.all([promise1, promise2, promise3]) .then((response) => {
+//    console.log(response);
+// })
+//
+// .catch(error => console.log(error))
+
+// Promise.race([promise1, promise2, promise3]).then((value) => {
+//    console.log(value);
+//  })
+//  Promise.race([promise1, promise2]).then((value) => {
+   //    console.log(value);
+   //  });
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// Promise.any([promise1(), promise2(), promise3()])
+// .then((any)=>{console.log(any);})
 
 
 
+// const promise1 = new Promise((resolve, reject) => {
+//    setTimeout(resolve, 500, 'one');
+//  });
+ 
+//  const promise2 = new Promise((resolve, reject) => {
+//    setTimeout(resolve, 100, 'two');
+//  });
+ 
+//  
+
+// 
+//       
+
+function raceTimeout(fetchDataPromise, timeout){
+   return new Promise((resolve, reject) => {
+      if (fetchDataPromise.status === 'FULLFILD') {
+         setTimeout(resolve(fetchDataPromise), timeout)
+      }else{
+         reject('Запит неуспішний')
+      }
+   })
+}
+
+const fetchDataPromise = fetch('https://api.example.com/data');
+const timeout = 5000;
+raceTimeout(fetchDataPromise, timeout)
+.then((data) => {console.log(data);})
+.catch((error => {console.log(error);})) 
--- src/timer.js
+++ src/timer.js
@@ -1,50 +1,50 @@
-const refs = {
-    startBtn: document.querySelector('button[data-action-start]'),
-    stopBtn: document.querySelector('button[data-action-stop]'),
-    clockface: document.querySelector('.js-clockface'),
-};
+// const refs = {
+//     startBtn: document.querySelector('button[data-action-start]'),
+//     stopBtn: document.querySelector('button[data-action-stop]'),
+//     clockface: document.querySelector('.js-clockface'),
+// };
 
 
 
-const timer = {
-    timerInterval:null,
-    start(){
-        const startTime = Date.now()
-        this.timerInterval = setInterval(()=>{
-            const currentTime = Date.now()
-            const deltaTime = currentTime - startTime
+// const timer = {
+//     timerInterval:null,
+//     start(){
+//         const startTime = Date.now()
+//         this.timerInterval = setInterval(()=>{
+//             const currentTime = Date.now()
+//             const deltaTime = currentTime - startTime
         
 
-        const time = getTimeComponents(deltaTime)
+//         const time = getTimeComponents(deltaTime)
         
-        updateClockface(time)
-        refs.startBtn.disabled = true
-        }, 1000)
-    },
-    stop(){
-        clearInterval(this.timerInterval)
-        refs.startBtn.disabled = false
-    }
-}
+//         updateClockface(time)
+//         refs.startBtn.disabled = true
+//         }, 1000)
+//     },
+//     stop(){
+//         clearInterval(this.timerInterval)
+//         refs.startBtn.disabled = false
+//     }
+// }
 
-function pad(value) {
-    return String(value).padStart(2, '0');
-  }
+// function pad(value) {
+//     return String(value).padStart(2, '0');
+//   }
 
 
-function getTimeComponents(time) {
-    const hours = pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    );
-    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+// function getTimeComponents(time) {
+//     const hours = pad(
+//       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+//     );
+//     const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//     const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-    return { hours, mins, secs };
-  }
+//     return { hours, mins, secs };
+//   }
 
-function updateClockface({ hours, mins, secs }) {
-    refs.clockface.textContent = `${hours}:${mins}:${secs}`;
-  }
+// function updateClockface({ hours, mins, secs }) {
+//     refs.clockface.textContent = `${hours}:${mins}:${secs}`;
+//   }
 
-refs.startBtn.addEventListener('click', ()=>{timer.start()})
-refs.stopBtn.addEventListener('click', ()=>{timer.stop()})
+// refs.startBtn.addEventListener('click', ()=>{timer.start()})
+// refs.stopBtn.addEventListener('click', ()=>{timer.stop()})
